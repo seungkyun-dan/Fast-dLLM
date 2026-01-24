167bc60e200b1b85a5396456a2c3c52f13d5f56c
Seungkyun Dan <ekstmdrbs@gmail.com>
2026-01-24 16:26:32 +0900

    forward pass for visualization

---
+++ llada/eval_vanilla.sh
@@ -0,0 +1,19 @@
+# Set the environment variables first before running the command.
+export HF_ALLOW_CODE_EVAL=1
+export HF_DATASETS_TRUST_REMOTE_CODE=true
+
+task=gsm8k
+length=256
+block_length=32
+num_fewshot=5
+steps=$((length / block_length))
+factor=1.0
+model_path='GSAI-ML/LLaDA-8B-Instruct'
+# You can change the model path to LLaDA-1.5 by setting model_path='GSAI-ML/LLaDA-1.5'
+
+
+# baseline
+accelerate launch eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
+--confirm_run_unsafe_code --model llada_dist \
+--model_args model_path=${model_path},gen_length=${length},steps=${length},block_length=${block_length},show_speed=True \
+--limit 1
